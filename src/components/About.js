46d81f1ab00bbd5ba3d7e5d5fe060a07c934f2af
Alex <doll3098@ou.edu>
2018-06-01 02:32:14 +0000

    Finish styles on homepage and add basic styles to About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../stylesheets/About.css';
 
 const About = () => (
-  <div>
+  <div className='about'>
     <h1>Hi there! This is the About page!</h1>
     <p>I need to fill this page out later with text about the page. See you then!</p>
-    <Link to='/'>Back to the soundboard</Link>
+    <Link className='button' to='/'>Back to the soundboard</Link>
   </div>
 );
 
